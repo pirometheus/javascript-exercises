66250a9e6a26879affc2647f1f869f207121a032
pirometheus <pirsu.turan@gmail.com>
2024-09-25 00:30:36 +0200

    my solution

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
-};
+const repeatString = function(string,num) {
+    let newString=string;
+    if (num==0){
+        return "";
+    } else if (num<0){
+        return "ERROR";
+    }
+    for (i=1;i<num;i++) {
+      newString=newString+string
+    }
+    return newString;  
+  }
+repeatString('hey',3);
 
 // Do not edit below this line
 module.exports = repeatString;
