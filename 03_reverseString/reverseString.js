2a12df8c520e6bede776b6bcb23f9ce7ed011a21
pirometheus <pirsu.turan@gmail.com>
2024-09-26 21:27:33 +0200

    reversed with .at()

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
-};
+const reverseString=function(string){
+    const num=string.length;
+    let newString="";
+    for (i=1;i<=num;i++){
+      newString+= string.at(num-i);
+    }
+    return newString;
+  }
 
 // Do not edit below this line
 module.exports = reverseString;
