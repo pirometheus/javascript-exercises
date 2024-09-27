24c91f5c3a98cdd8e9ce1d63f2ab863f698b22cb
pirometheus <pirsu.turan@gmail.com>
2024-09-27 22:23:13 +0200

    fixed: doesn't work for multiple arguments

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,17 +1,18 @@
-const removeFromArray = function(array,argument1,argument2) {
-  
-    let lenth=array.length;
-    for (i=0;i<lenth;i++){
-      let index1=array.indexOf(argument1);
-      if (index1>-1) {
-        array.splice(index1,1);
-      }
-      let index2=array.indexOf(argument2);
-      if (index2>-1) {
-        array.splice(index2,1);
+const removeFromArray = function(array,...func) {
+    let restParameter=func; // create an new array with arguments 
+    let uzunluk=restParameter.length
+    for (j=0;j<uzunluk;j++){ // loops every arguments 
+      let argument=restParameter[j]
+      let lenth=array.length;
+      for (i=0;i<lenth;i++){ //removes the argument from given array
+        let index1=array.indexOf(argument);
+        if (index1>-1) {
+          array.splice(index1,1);
+        }
       }
     }
     return array;
+  
   }
 
 // Do not edit below this line
