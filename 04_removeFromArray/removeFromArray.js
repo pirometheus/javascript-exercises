d89cb9a5d81ffbdbde61c45b64b49cfcce3a0322
pirometheus <pirsu.turan@gmail.com>
2024-09-27 21:41:26 +0200

    done except multivariable case

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array,argument1,argument2) {
+  
+    let lenth=array.length;
+    for (i=0;i<lenth;i++){
+      let index1=array.indexOf(argument1);
+      if (index1>-1) {
+        array.splice(index1,1);
+      }
+      let index2=array.indexOf(argument2);
+      if (index2>-1) {
+        array.splice(index2,1);
+      }
+    }
+    return array;
+  }
 
 // Do not edit below this line
 module.exports = removeFromArray;
