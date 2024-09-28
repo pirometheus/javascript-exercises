62fb72641e50391f22202ce1ecece63bf7dd93b1
pirometheus <pirsu.turan@gmail.com>
2024-09-28 21:07:49 +0200

    problem solved

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,22 @@
-const convertToCelsius = function() {
+const convertToFahrenheit = function(tempInFahrenheit) {
+  let convInCelcius = (9*tempInFahrenheit)/5 +32;
+  return round(convInCelcius);
 };
 
-const convertToFahrenheit = function() {
+const convertToCelsius = function(tempInCelsius) {
+  let convInFahrenheit = (tempInCelsius-32)*(5/9);
+  return round(convInFahrenheit);
 };
+
+function round(number){
+  if (Number.isInteger(number)){
+      return number;
+  } else {
+    newNumb=+number.toFixed(1); 
+    return newNumb;
+  }
+};
+
 
 // Do not edit below this line
 module.exports = {
